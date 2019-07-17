49b7a65037ea6667bac9deef987ba2bb38b48f14
Chris York <christopherayork1992@gmail.com>
2019-07-16 23:19:09 -0400

    Props addded, App.js built out, hooks partially done, sass finished, starting functionality

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
   return (
-    <>
+    <button className={`numberButton ${props.number === '0' ? 'grow' : ''}`}>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.number}
+    </button>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
+import OperatorButton from './OperatorButton.js';
 
 //Import your array data to from the provided data file
+import { operators } from '../../../data.js';
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  let [operatorState, setOperator] = useState(operators);
   return (
-    <div>
+    <div className={'operators'}>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {operators.map(o => <OperatorButton operator={o.value} char={o.char} />)}
     </div>
   );
 };
+
+export default Operators;
